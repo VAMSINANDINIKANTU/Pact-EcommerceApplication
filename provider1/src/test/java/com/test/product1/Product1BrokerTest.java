1d76d230aab1e8a86927c37b7d9aacfabfe1490e
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-07-15 18:05:23 +0530

    second commit

--- provider1/src/test/java/com/test/product1/Product1BrokerTest.java
+++ provider1/src/test/java/com/test/product1/Product1BrokerTest.java
@@ -25,7 +25,7 @@ public class Product1BrokerTest {
 
 	@TestTarget
 	public final Target target = new HttpTarget("http", "localhost", PORT);
-    private static ConfigurableApplicationContext applicationContext;
+        private static ConfigurableApplicationContext applicationContext;
 
 	@BeforeClass
 	public static void setVersion() {
@@ -39,7 +39,7 @@ public class Product1BrokerTest {
 	}
 
 	@AfterClass
-    public static void stop() {
+        public static void stop() {
 		SpringApplication.exit(applicationContext);
 	}
 
