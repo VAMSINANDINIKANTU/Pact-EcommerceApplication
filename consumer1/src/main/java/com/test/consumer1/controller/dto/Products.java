64d161e89c102b1da4950a108c1c1067cccc5b33
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-07-15 17:55:10 +0530

    second commit

--- consumer1/src/main/java/com/test/consumer1/controller/dto/Products.java
+++ consumer1/src/main/java/com/test/consumer1/controller/dto/Products.java
@@ -1,19 +1,14 @@
 package com.test.consumer1.controller.dto;
-
 import java.io.Serializable;
 import java.util.List;
 
 public class Products implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
+        private static final long serialVersionUID = 1L;
 	private List<ProductsVO> products;
-
-	public List<ProductsVO> getProductsVOs() {
+        public List<ProductsVO> getProductsVOs() {
 		return products;
 	}
-
-	public void setProductsVOs(List<ProductsVO> products) {
+        public void setProductsVOs(List<ProductsVO> products) {
 		this.products = products;
 	}
 }
